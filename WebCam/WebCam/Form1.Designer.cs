3a408747f918ed184a9987a62addd78004de01c1
Сахиб Гаджиев <Altair96.96@mail.ru>
2017-05-31 20:47:26 +0300

    FaceRecog

---
+++ WebCam/WebCam/Form1.Designer.cs
@@ -0,0 +1,352 @@
+﻿namespace WebCam
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.camera1FpsLabel = new System.Windows.Forms.Label();
+            this.camera1Combo = new System.Windows.Forms.ComboBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.WaitInfo = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.buttonLearn = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.OldBox = new System.Windows.Forms.TextBox();
+            this.SurnameBox = new System.Windows.Forms.TextBox();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.IDBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.videoSourcePlayer1.ForeColor = System.Drawing.Color.White;
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(10, 50);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(322, 242);
+            this.videoSourcePlayer1.TabIndex = 0;
+            this.videoSourcePlayer1.VideoSource = null;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.camera1FpsLabel);
+            this.groupBox1.Controls.Add(this.camera1Combo);
+            this.groupBox1.Controls.Add(this.videoSourcePlayer1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(342, 315);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "CAMERA";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // camera1FpsLabel
+            // 
+            this.camera1FpsLabel.Location = new System.Drawing.Point(282, 295);
+            this.camera1FpsLabel.Name = "camera1FpsLabel";
+            this.camera1FpsLabel.Size = new System.Drawing.Size(50, 17);
+            this.camera1FpsLabel.TabIndex = 4;
+            this.camera1FpsLabel.Text = "label1";
+            this.camera1FpsLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // camera1Combo
+            // 
+            this.camera1Combo.FormattingEnabled = true;
+            this.camera1Combo.Location = new System.Drawing.Point(10, 20);
+            this.camera1Combo.Name = "camera1Combo";
+            this.camera1Combo.Size = new System.Drawing.Size(322, 21);
+            this.camera1Combo.TabIndex = 3;
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(170, 335);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "&Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(267, 335);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 5;
+            this.stopButton.Text = "S&top";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tabControl1);
+            this.panel1.Location = new System.Drawing.Point(368, 29);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(434, 328);
+            this.panel1.TabIndex = 6;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Location = new System.Drawing.Point(0, 3);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(491, 325);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.WaitInfo);
+            this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.textBox1);
+            this.tabPage1.Controls.Add(this.buttonLearn);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.OldBox);
+            this.tabPage1.Controls.Add(this.SurnameBox);
+            this.tabPage1.Controls.Add(this.NameBox);
+            this.tabPage1.Controls.Add(this.IDBox);
+            this.tabPage1.Controls.Add(this.label5);
+            this.tabPage1.Controls.Add(this.label4);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Controls.Add(this.label2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(483, 299);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Controll Page";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // WaitInfo
+            // 
+            this.WaitInfo.AutoSize = true;
+            this.WaitInfo.Location = new System.Drawing.Point(306, 264);
+            this.WaitInfo.Name = "WaitInfo";
+            this.WaitInfo.Size = new System.Drawing.Size(85, 13);
+            this.WaitInfo.TabIndex = 17;
+            this.WaitInfo.Text = "System no ready";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(21, 188);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(84, 13);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Images directory";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 182);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // buttonLearn
+            // 
+            this.buttonLearn.Location = new System.Drawing.Point(306, 224);
+            this.buttonLearn.Name = "buttonLearn";
+            this.buttonLearn.Size = new System.Drawing.Size(75, 23);
+            this.buttonLearn.TabIndex = 14;
+            this.buttonLearn.Text = "learn";
+            this.buttonLearn.UseVisualStyleBackColor = true;
+            this.buttonLearn.Click += new System.EventHandler(this.CLickForLearn);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(114, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(111, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "data base  connected";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(306, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "delete";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(306, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "add";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // OldBox
+            // 
+            this.OldBox.Location = new System.Drawing.Point(117, 143);
+            this.OldBox.Name = "OldBox";
+            this.OldBox.Size = new System.Drawing.Size(100, 20);
+            this.OldBox.TabIndex = 10;
+            // 
+            // SurnameBox
+            // 
+            this.SurnameBox.Location = new System.Drawing.Point(117, 116);
+            this.SurnameBox.Name = "SurnameBox";
+            this.SurnameBox.Size = new System.Drawing.Size(100, 20);
+            this.SurnameBox.TabIndex = 9;
+            this.SurnameBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(117, 90);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(100, 20);
+            this.NameBox.TabIndex = 8;
+            // 
+            // IDBox
+            // 
+            this.IDBox.Location = new System.Drawing.Point(117, 65);
+            this.IDBox.Name = "IDBox";
+            this.IDBox.Size = new System.Drawing.Size(100, 20);
+            this.IDBox.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(65, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(23, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Old";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Surname";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(53, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 369);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "WebCamApp";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox camera1Combo;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label camera1FpsLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox OldBox;
+        private System.Windows.Forms.TextBox SurnameBox;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox IDBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button buttonLearn;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label WaitInfo;
+    }
+}
+
---
+++ WebCam/WebCam/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+//using System.Drawing;
+using System.Drawing.Imaging;
+using AForge.Video;
+using AForge.Video.VFW;
+using AForge.Controls;
+using AForge.Video.DirectShow;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.VideoSurveillance;
+using Emgu.CV.Cvb;
+using Emgu.CV.UI;
+using Emgu.CV.Face;
+
+namespace WebCam
+{
+    public partial class Form1 : Form
+    {
+
+        //AVIReader reader;
+         // list of video devices
+        FilterInfoCollection videoDevices;
+        // stop watch for measuring fps
+        private Stopwatch stopWatch = null;
+        private CascadeClassifier faceCascade;
+        FaceRecognition faceRecog;
+        String[] mas= {"Unknow","Чел 1 ","Чел 2 ","Чел 3 ","Чел 4 ","Чел 5 "," Gadjiev Saxib 21 year old, " };
+        public Form1( )
+        {
+
+            //reader = new AVIReader();
+            //reader.Open("123.avi");
+            //Capture 
+            faceRecog = new FaceRecognition();
+            InitializeComponent( );
+             faceCascade = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        //  button2.Enabled = false;
+        //   button3.Enabled = false;
+            // buttonLearn.Enabled = false;
+             LBPHFaceRecognizer recognizer = new LBPHFaceRecognizer(1,8,8,8,123);
+             
+
+            camera1FpsLabel.Text = string.Empty;
+            videoSourcePlayer1.NewFrame += new VideoSourcePlayer.NewFrameHandler(this.playerControl_NewFrame);
+         
+
+            // show device list
+			try
+			{
+                // enumerate video devices
+                videoDevices = new FilterInfoCollection( FilterCategory.VideoInputDevice );
+
+                if ( videoDevices.Count == 0 )
+                {
+                    throw new Exception( );
+                }
+
+                for ( int i = 1, n = videoDevices.Count; i <= n; i++ )
+                {
+                    string cameraName = i + " : " + videoDevices[i - 1].Name;
+
+                    camera1Combo.Items.Add( cameraName );
+                    
+                }
+             //camera1Combo.Items.Add("2 : WebCam GG-321");
+             //camera1Combo.Items.Add("3 : WebCam SC-01DCL32219N");
+             // check cameras count
+               
+                camera1Combo.SelectedIndex = 0;
+            }
+            catch
+            {
+                startButton.Enabled = false;
+
+                camera1Combo.Items.Add( "No cameras found" );
+               
+
+                camera1Combo.SelectedIndex = 0;
+               
+
+                camera1Combo.Enabled = false;
+              
+            }
+        }
+
+        // On form closing
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StopCameras( );
+        }
+
+        // On "Start" button click
+        private void startButton_Click( object sender, EventArgs e )
+        {
+            StartCameras( );
+
+            startButton.Enabled = false;
+            stopButton.Enabled = true;
+        }
+
+        // On "Stop" button clickpla
+        private void stopButton_Click( object sender, EventArgs e )
+        {
+           // if (bitmap != null)
+              //  bitmap.Save("123.png", ImageFormat.Png);
+            StopCameras( );
+            
+
+            startButton.Enabled = true;
+            stopButton.Enabled = false;
+
+            camera1FpsLabel.Text = string.Empty;
+           
+        }
+
+        // Start cameras
+        private void StartCameras( )
+        {
+            // create first video source
+            VideoCaptureDevice videoSource1 = new VideoCaptureDevice(videoDevices[camera1Combo.SelectedIndex].MonikerString );
+            
+           
+            //videoSource1.NewFrame += new NewFrameEventHandler(video_NewFrame);
+            videoSourcePlayer1.VideoSource = videoSource1;
+           
+            videoSourcePlayer1.Start( );
+
+            // create second video source
+           
+
+            // reset stop watch
+            stopWatch = null;
+            // start timer
+            timer.Start( );
+        }
+
+        // Stop cameras
+        private void StopCameras( )
+        {
+            timer.Stop( );
+
+            videoSourcePlayer1.SignalToStop( );
+            
+
+            videoSourcePlayer1.WaitForStop( );
+           
+        }
+
+        Bitmap bitmap=null;
+      
+        // new frame event handler
+        private void playerControl_NewFrame(object sender, ref Bitmap image)
+        {
+           // image = reader.GetNextFrame();
+            Image<Gray, Byte> tempImage = new Image<Gray, Byte>(image);
+         //   Mat sharp = new Mat(m);
+            var Face = faceCascade.DetectMultiScale(tempImage, 1.1, 6, new Size(90, 90));
+            FaceRecognizer.PredictionResult k;
+            int f;
+            Font drawFont = new Font("Arial", 20);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            foreach (var face in Face)
+            { 
+                Graphics.FromImage(image).DrawRectangle(new Pen(Brushes.Black, 5), face);
+                if (flag == true)
+                {
+                    k = faceRecog.Recognition(tempImage.Copy(face));
+                    if (k.Distance > 75)
+                    {
+                        f = 0;
+                        drawBrush = new SolidBrush(Color.Red);
+                    }
+                    else
+                        f = k.Label;
+                    PointF drawPoint = new PointF(face.X, face.Y-30);
+                Graphics.FromImage(image).DrawString(mas[f]+k.Distance, drawFont, drawBrush,drawPoint);
+                }
+               
+               
+
+            }
+           
+           
+        }
+       // private void video_NewFrame(object sender, NewFrameEventArgs eventArgs)
+       // {
+            
+           // bitmap =new Bitmap( eventArgs.Frame);
+           
+      //  }
+        // On times tick - collect statistics
+        private void timer_Tick( object sender, EventArgs e )
+        {
+            IVideoSource videoSource1 = videoSourcePlayer1.VideoSource;
+
+            
+            int framesReceived1 = 0;
+            int framesReceived2 = 0;
+
+            // get number of frames for the last second
+            if ( videoSource1 != null )
+            {
+                framesReceived1 = videoSource1.FramesReceived;
+              
+            
+               
+            }
+
+           
+
+            if ( stopWatch == null )
+            {
+                stopWatch = new Stopwatch( );
+                stopWatch.Start( );
+            }
+            else
+            {
+                stopWatch.Stop( );
+
+                float fps1 = 1000.0f * framesReceived1 / stopWatch.ElapsedMilliseconds;
+                float fps2 = 1000.0f * framesReceived2 / stopWatch.ElapsedMilliseconds;
+
+                camera1FpsLabel.Text = fps1.ToString( "F2" ) + " fps";
+               
+
+                stopWatch.Reset( );
+                stopWatch.Start( );
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //flag = true;
+            if (bitmap != null )
+                bitmap.Save("123.jpg", ImageFormat.Png);
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Connect(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        bool flag = false;
+        private void CLickForLearn(object sender, EventArgs e)
+        {
+            WaitInfo.Text = "" + "wait/ system learning";
+
+            faceRecog.FaceLearn();
+            flag = true;
+            WaitInfo.Text = "" + "system is ready";
+        }
+       // public Form1()
+      //  {
+           // InitializeComponent();
+       // }
+    }
+}
